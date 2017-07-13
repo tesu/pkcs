8d05425ac227dc5bb508c2ff1bf084fbaee7493e
tesu <jasonlam@mit.edu>
2017-07-13 17:39:45 -0400

    more info in blurb

--- imports/ui/game.js
+++ imports/ui/game.js
@@ -2,6 +2,10 @@ import {Template} from 'meteor/templating';
 
 import './game.html';
 
+function cap(s) {
+    if (s.length < 1) return '';
+    return s[0].toUpperCase() + s.slice(1);
+}
 Template.game.helpers({
     playerCount() {
         return this.players.length || 0;
@@ -10,6 +14,11 @@ Template.game.helpers({
         const user = Meteor.users.findOne({_id: this.host});
         if (user) return user.username;
         return null
+    },
+    blurb() {
+        const user = Meteor.users.findOne({_id: this.host});
+        if (!user) return null;
+        return cap(this.rank) + ' ' + cap(this.category) + ' contest hosted by ' + user.username
     },
 });
 
